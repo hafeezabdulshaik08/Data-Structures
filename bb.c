1f5862a4539964e33163d86303fce62631898219
hafeezabdulshaik08 <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-05-04 15:23:38 +0530

    Add files via upload

---
+++ bb.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 20
+char stack[MAX];
+int top=-1;
+void push(char item){
+	stack[++top]=item;
+}
+int isMatch(char ch1,char ch2){
+	if(ch1=='(' && ch2==')') return 1;	
+	else if(ch1=='[' && ch2==']') return 1;	
+	else if(ch1=='{' && ch2=='}') return 1;	
+	else return 0;
+}
+char pop(){
+	char ch;
+	if(top==-1){
+		printf("Not balenced\n");
+		exit (0);
+	}
+	ch=stack[top];
+	top--;
+	return ch;
+}
+int isEmpty(){
+	if(top==-1) return 1;
+	else return 0;
+}
+int main(){
+	char exp[MAX],ch,temp;
+	int i;
+	printf("enter the expression\n");
+	scanf("%s",exp);
+	for(i=0;exp[i]!='\0';i++){
+		ch=exp[i];
+		if(ch=='('||ch=='['||ch=='{')
+			push(ch);
+		else{
+			temp=pop();
+			if(!isMatch(temp,ch)){
+				printf("not balanced\n");
+				exit(0);
+			}
+		}
+	}
+	if(isEmpty()) printf("Balanced\n");
+	else printf("Not balanced\n");
+	return 0;
+}									
